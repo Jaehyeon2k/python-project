23a18b576f69cc7fa3f385527493404f7d87e99c
김재현 <wmspfl2673@gmail.com>
2025-08-29 10:20:12 +0900

    0829 first commit

---
+++ 0829/hello.py
@@ -0,0 +1,25 @@
+# message = 'hello'
+# value = 10
+# result = value + 10
+# checked = True
+# print(result)
+# print(type(checked))
+# print(value, message)
+
+# strValue = '1'
+# value2 =  int(strValue)
+# print(type(value2), type(strValue))
+
+# value3 = 2
+# str2 = str(value3)
+# print(type(str2), type(value3))
+
+value1 = 1
+value2 = 1
+value3 = value1 + value2
+print(value3)
+
+str1 = "1"
+str2 = "1"
+str3 = str1 + str2
+print(str3)
